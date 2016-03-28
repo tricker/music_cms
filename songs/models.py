5daeb50d219cb5ff015fe27242a01000f9d3eba7
Todd Ricker <Todd_Ricker@cable.comcast.com>
2016-03-28 18:16:13 -0400

    added Album model

--- songs/models.py
+++ songs/models.py
@@ -2,4 +2,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Album(models.Model):
+    album_name = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date released')
+
+    def __str__(self):
+        return self.album_name
